b72b3c249895c431514eb9f15b46ae73a5b200e6
fyzalis <julien.buabent@gmail.com>
2021-04-08 20:23:30 +0200

    rajoute commentaire pédagogique

--- save.php
+++ save.php
@@ -1,4 +1,9 @@
 <?
-require "class_score.php";
-$score = new Score();
-$score->save($_POST['pseudo'], $_POST['time'], $_POST['level']);
+//Ce fichier recoit le form qui sert à enregistrer un score
+
+require "class_score.php"; //On appel la classe score
+$score = new Score(); //On l'instancie dans un objet
+
+//FILTERINPUTVAR :X
+
+$score->save($_POST['pseudo'], $_POST['time'], $_POST['level']); //On exécute la méthode save
